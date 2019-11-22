44a285d62c88d78938ffc94d318dc9c78dbd52e1
Asher Lieber <asherlie@buffalo.edu>
2019-11-22 18:37:45 -0500

    defined datatypes to eep track of gcode operations and functions to process them

--- ogg.hs
+++ ogg.hs
@@ -4,23 +4,44 @@
  - Gcode
  -}
 
-import System.Environment
-data MY_BOOL =
-    T
-  | F
+data CMD_PRE = LAYER | G | M
+data CMD = CMD CMD_PRE Integer Integer | RAW_GCODE String | CMD_EMPTY
 
-  deriving (Show)
+chunk :: String -> [String]
+chunk str = case (words str) of
+                  x:y:z:rest   -> [x, y, z]
+                  x:y:rest     -> [x, y, ""]
+                  x:rest       -> [x, "", ""]
+                  _            -> ["", "", ""]
 
-xor :: MY_BOOL ->  MY_BOOL -> MY_BOOL
-xor a b =
-      case a of
-            T -> case b of
-                  F -> T
-                  T -> F
-            F -> case b of
-                  T -> T
-                  F -> F
-            
-main = 
-      {-print (xor (F, T))-}
-      print (xor F T)
+flatten :: [String] -> String
+flatten (x:y:rest) = x ++ y ++ (flatten rest)
+flatten [x]        = x
+flatten []         = ""
+
+numeric :: String -> Bool
+numeric (x:rest) = elem x "0123456789" && (numeric rest)
+numeric [] = True
+
+--process_cmd :: String -> [String]
+-- takes in a string broken up by spaces
+process_cmd :: [String] -> CMD
+process_cmd ["l", x, y]  = CMD LAYER (read x :: Integer) (read y :: Integer)
+process_cmd ["g", x, y]  = CMD G (read x :: Integer) (read y :: Integer)
+process_cmd ["", "", ""] = CMD_EMPTY
+process_cmd lst  = RAW_GCODE (flatten lst)
+
+eval_cmd :: CMD -> IO ()
+eval_cmd (CMD prefix x y) = case prefix of
+                              LAYER -> putStrLn ("layer operation in " ++ show x)
+                              G     -> putStrLn ("G" ++ show x)
+                              M     -> putStrLn ("M command" ++ show x)
+
+eval_cmd (RAW_GCODE cmdstr) = putStrLn cmdstr
+eval_cmd (CMD_EMPTY) = putStrLn "try again"
+
+
+main = do
+      name <- getLine
+      --x <- process_cmd (chunk name)
+      putStr name
